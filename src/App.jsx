282bb9d9e99210e72d4dc6ce2df14d28c92afa27
nimeshasewmini <123630982+nimeshasewmini@users.noreply.github.com>
2025-03-05 02:36:07 +0530

    portfolio until contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import Test from "./Test";
+import "./app.scss"
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/Parallax/Parallax";
+import Services from "./components/sevices/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Homepage">
+      <Navbar/>
+      <Hero/>  
+    </section>
+    <section id="Services">
+      <Parallax type="services"/>
+    </section>
+    <section>
+        <Services/>
+    </section>
+    <section id="Portfolio">
+      <Parallax type="portfolio"/>
+    </section>
+    <Portfolio/>
+   
+  
+    <section id="Contact"><Contact/></section>
+    {/* <Test/> */}
+  </div>;
 };
 
 export default App;
